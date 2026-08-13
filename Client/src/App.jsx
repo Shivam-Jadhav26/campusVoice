3c85559bccb85dd000fdf7ab4d2952e72a515d3d
shivam Jadhav <shivamaj2005@gmail.com>
2026-08-13 17:21:20 +0530

    folder format

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Toaster } from "react-hot-toast"
+import LoginLanding from './pages/LoginLanding'
+import Dashboard from './pages/Dashboard'
+import Employee from './pages/Employee'
+import Attendence from './pages/Attendence'
+import Payslip from './pages/Payslip'
+import Setting from './pages/Setting'
+import PrintPayslip from './pages/PrintPayslip'
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <>
+    <Toaster/>
+    <Routes>
+      <Route path = "/login" element={<LoginLandin/>}/> 
+      <Route element={<Layout/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>} /> 
+        <Route path="/employee" element={<Employee />} /> 
+        <Route path="/attendence" element={<Attendence />} /> 
+        <Route path="/payslip" element={<Payslip />} /> 
+        <Route path="/setting" element={<Setting />} /> 
+
+    </Routes>
+      <Route path="/print/payslips/:id" element={<PrintPayslip />} /> 
+      <Route path="*" element={<Navigate to = "Dashboard" replace />} /> 
+
+    </>
   )
 }
 
---
+++ Client/src/pages/LoginLanding.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginLanding = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginLanding
